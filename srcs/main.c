77fdc59e793c2a3bf9a255da104a2be7d1702328
Anthony COLAS <anthonycolas.france@gmail.com>
2019-03-27 22:08:03 +0100

    Main et lem_in.h

--- srcs/main.c
+++ srcs/main.c
@@ -6,21 +6,59 @@
 /*   By: acolas <acolas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/27 11:49:55 by acolas            #+#    #+#             */
-/*   Updated: 2019/03/27 16:18:49 by acolas           ###   ########.fr       */
+/*   Updated: 2019/03/27 22:07:30 by acolas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "lemin.h"
+#include "lem_in.h"
+
+int		parse_params(char **argv)
+{
+	if (!argv[1])
+		return (0);
+	if (!ft_strcmp(argv[1], "--leaks"))
+		return (1);
+	else if (!ft_strcmp(argv[1], "--silent"))
+		return (2);
+	else if (!ft_strcmp(argv[1], "--paths"))
+		return (3);
+	else if (!ft_strcmp(argv[1], "--moves"))
+		return (4);
+	else
+		return (0);
+}
+
+t_list	*add_to_the_end_of_list(t_list *head, t_list *new)
+{
+	t_list	*crawler;
+
+	crawler = head;
+	if (!crawler)
+		return (new);
+	while (crawler = crawler->next)
+		crawler = crawler->next;
+	crawler->next = new;
+	return (head);
+}
 
 int		main(int ac, char **av)
 {
-	if (ac == 4)
+	char	*line;
+	t_list	*map;
+	t_list	*new;
+	int		params;
+
+	while (get_next_line(0, &line) > 0)
 	{
-		//count_ants(av[1]);
-		//parse_map(av[2], av[3]);
-		ft_printf(av[1]);
+		new = ft_lstnew(NULL, 0);
+		new->content = line;
+		map = add_to_the_end_of_list(map, new);
 	}
-	else
-		ft_printf("ERROR");
-	return (0);
+	argc ? (params = parse_params(argv)) :
+		(params = 0);
+	if (!map)
+		put_err_msg_exit("Usage: You need to give a valid map");
+	validate(map, params);
+	if (params == 1)
+		system("leaks lem-in");
 }
