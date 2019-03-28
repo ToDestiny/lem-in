70fa759b43d9c82886af40fcb747a15df3a6498b
Anthony COLAS <acolas@e2r4p8.42.fr>
2019-03-28 14:37:05 +0100

    struct lem_in.h and argv

--- includes/lem_in.h
+++ includes/lem_in.h
@@ -1,19 +1,51 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   lemin.h                                            :+:      :+:    :+:   */
+/*   lem_in.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: acolas <acolas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/27 11:46:52 by acolas            #+#    #+#             */
-/*   Updated: 2019/03/27 21:51:01 by acolas           ###   ########.fr       */
+/*   Updated: 2019/03/28 14:36:24 by acolas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LEMIN_H
 # define LEMIN_H
 # include "../libft/libft.h"
+# define ERROR_ROOMS "Error: Invalid map (more than one start/end, etc..)"
+
+typedef struct 		s_room
+{
+		char			*name;
+		int				is_start;
+		int				is_end;
+		struct s_room	*from;
+		t_list			*links;
+		int				x;
+		int				y;
+		int				is_visited;
+		int				is_closed;
+		int				ant;
+}					t_room;
+
+typedef	struct 		s_link
+{
+		t_room			*r1;
+		t_room			*r2;
+};					t_link;
+
+typedef	struct 		s_ant
+{
+		int				ant;
+		int				end;
+		t_list			*path;
+};					t_ant;
+
 
 int				ft_printf(const char *fm, ...);
+t_list			*add_to_the_end_of_list(t_list *head, t_list *new);
+int				parse_params(char **av);
+
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -6,23 +6,23 @@
 /*   By: acolas <acolas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/27 11:49:55 by acolas            #+#    #+#             */
-/*   Updated: 2019/03/27 22:07:30 by acolas           ###   ########.fr       */
+/*   Updated: 2019/03/28 14:36:30 by acolas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 
-int		parse_params(char **argv)
+int		parse_params(char **av)
 {
-	if (!argv[1])
+	if (!av[1])
 		return (0);
-	if (!ft_strcmp(argv[1], "--leaks"))
+	if (!ft_strcmp(av[1], "--leaks"))
 		return (1);
-	else if (!ft_strcmp(argv[1], "--silent"))
+	else if (!ft_strcmp(av[1], "--silent"))
 		return (2);
-	else if (!ft_strcmp(argv[1], "--paths"))
+	else if (!ft_strcmp(av[1], "--paths"))
 		return (3);
-	else if (!ft_strcmp(argv[1], "--moves"))
+	else if (!ft_strcmp(av[1], "--moves"))
 		return (4);
 	else
 		return (0);
@@ -54,7 +54,7 @@ int		main(int ac, char **av)
 		new->content = line;
 		map = add_to_the_end_of_list(map, new);
 	}
-	argc ? (params = parse_params(argv)) :
+	argc ? (params = parse_params(av)) :
 		(params = 0);
 	if (!map)
 		put_err_msg_exit("Usage: You need to give a valid map");
